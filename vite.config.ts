a10f83af0598cce9846ef0cf41f7e13fa4084935
Josiah Ayres <josiah@lend.com.au>
2022-12-18 17:23:37 +1300

    Fix pages dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/lan-mk-3/",
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
   build: { outDir: "build" },
